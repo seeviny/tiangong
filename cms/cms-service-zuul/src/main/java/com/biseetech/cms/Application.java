53d4ea0d1c76278a06006c5047bce720b2f5f78f
Seeviny Wang <seeviny@Seevinys-MacBook-Pro.local>
2018-08-08 23:13:06 +0800

    ..

--- cms/cms-service-zuul/src/main/java/com/biseetech/cms/Application.java
+++ cms/cms-service-zuul/src/main/java/com/biseetech/cms/Application.java
@@ -1,16 +1,11 @@
 package com.biseetech.cms;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
-@EnableAutoConfiguration
+@SpringCloudApplication
 @EnableZuulProxy
-
-@EnableEurekaClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
